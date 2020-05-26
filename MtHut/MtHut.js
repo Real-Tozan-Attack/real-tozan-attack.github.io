afb3e98f997455774770314c9dbba081664b7dda
Real-Tozan-Attack <62943068+Real-Tozan-Attack@users.noreply.github.com>
2020-05-26 19:32:26 +0900

    Update MtHut.js

--- MtHut/MtHut.js
+++ MtHut/MtHut.js
@@ -111,7 +111,7 @@ var RTA_ReachedMtlist_marker = L.geoJson(RTA_ReachedMtlist_Deta, {
 
 
 var markers1 = L.markerClusterGroup({
- disableClusteringAtZoom:10,
+ disableClusteringAtZoom:12,
  maxClusterRadius: 10,
     });
 var RTA_ReachedMtlist_Layer = markers1.addLayer(RTA_ReachedMtlist_marker);
